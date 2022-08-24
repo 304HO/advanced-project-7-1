075659930c910bd34a5355250f2d2fbe54cd93b9
JSC <gbs04087@gmail.com>
2022-08-24 11:23:05 +0900

    FEAT: add routes

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,5 +1,7 @@
 import Albums from "./pages/Albums";
+import Error404 from "./pages/Error404";
 import Photos from "./pages/Photos";
+import TestErrorPage from "./pages/TestErrorPage";
 
 export type RouteType = {
   name: string;
@@ -20,6 +22,18 @@ const routes: Array<RouteType> = [
     key: "Photos",
     route: "/photos",
     component: <Photos />
+  },
+  {
+    name: "Error404",
+    key: "Error404",
+    route: "/404",
+    component: <Error404 />
+  },
+  {
+    name: "TestErrorPage",
+    key: "TestErrorPage",
+    route: "/TestErrorPage",
+    component: <TestErrorPage />
   }
 ];
 
