10f10596698145d8c38067971564ab95c3c7ec73
JSC <gbs04087@gmail.com>
2022-08-24 11:25:14 +0900

    FEAT: add axios interceptor, error redirect

--- client/src/apis/utils/index.ts
+++ client/src/apis/utils/index.ts
@@ -1,8 +1,31 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
+  baseURL: "https://jsonplaceholder.typicode.com123",
   withCredentials: true
 });
+
+axiosApi.interceptors.request.use(
+  function (config) {
+    console.log("before", config);
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+axiosApi.interceptors.response.use(
+  function (response) {
+    console.log("response", response);
+    return response;
+  },
+  function (error) {
+    window.location.href = "/404";
+    console.log("navigate after");
+    return Promise.reject(error);
+  }
+);
 
 export default axiosApi;
